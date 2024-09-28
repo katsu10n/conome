7c64a9b3bddc56e568876aff5420d9be4108af13
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-09-28 23:51:02 +0900

    db/add: テストデータを追加（投稿）

--- src/database/seeders/PostSeeder.php
+++ src/database/seeders/PostSeeder.php
@@ -4,14 +4,20 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PostSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('posts')->insert([
+            [
+                'id' => 99999,
+                'user_id' => 2,
+                'category_id' => 1,
+                'title' => 'ユーザー２の投稿',
+                'content' => 'ユーザー２の投稿内容',
+            ],
+        ]);
     }
 }
