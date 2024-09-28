0d850ab85344147389f6131a43f0dafb83954729
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-09-28 17:10:51 +0900

    db/add: モデルと関連ファイルと作成、テーブルにカラムを追加(categories, posts, comments, likes, follows)

---
+++ src/database/migrations/2024_09_28_070240_create_categories_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->integer('id')->primary()->autoIncrement();
+
+            $table->text('name');
+            $table->timestampsTz();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
