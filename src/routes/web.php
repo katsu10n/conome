c0922cd8416d7affe8e7b6b4ca61388271524ba9
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-09-29 23:15:45 +0900

    feat/chg: ログイン時のルートをpostsに変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,9 +2,13 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect()->route('posts.index');
+    }
     return view('pages.welcome');
 });
 
