d925aa7a28c8350ecf2a8939275ec56ab652da3f
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-10-04 12:42:23 +0900

    refactor: プロフィールのフォローモーダルを共通化

--- src/resources/views/components/users/user-list-modal.blade.php
+++ src/resources/views/components/users/user-list-modal.blade.php
@@ -1,4 +1,4 @@
-@props(['title', 'open'])
+@props(['title', 'users', 'isFollowerModal', 'open'])
 
 <div class="fixed inset-0 z-50 overflow-y-auto" role="dialog" aria-modal="true" x-show="{{ $open }}">
     <div class="flex min-h-screen items-end justify-center px-4 pb-20 pt-4 text-center sm:block sm:p-0">
@@ -7,26 +7,34 @@
             x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200"
             x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
             @click="{{ $open }} = false"></div>
-
         <span class="hidden sm:inline-block sm:h-screen sm:align-middle" aria-hidden="true">&#8203;</span>
-
         <div class="inline-block w-full transform overflow-hidden rounded-lg bg-white text-left align-bottom shadow-xl transition-all sm:my-8 sm:max-w-lg sm:align-middle"
             x-show="{{ $open }}" x-transition:enter="ease-out duration-300"
             x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200"
             x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            @click.away="{{ $open }} = false">
-            <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">
-                    {{ $title }}
-                </h3>
-                <div class="mt-2">
-                    {{ $content }}
+            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            <div class="bg-white p-6">
+                <h3 class="text-lg font-medium leading-6 text-gray-900">{{ $title }}</h3>
+                <div class="mt-4 space-y-4">
+                    @foreach ($users as $user)
+                        <div class="flex items-center justify-between">
+                            <div>
+                                <a class="font-semibold hover:underline"
+                                    href="{{ route('profile.show', $user->uid) }}">{{ $user->name }}</a><br>
+                                <a class="text-sm text-gray-600 hover:underline"
+                                    href="{{ route('profile.show', $user->uid) }}">{{ '@' . $user->uid }}</a>
+                            </div>
+                            @if (Auth::id() !== $user->id)
+                                <x-users.user-follow-button :user="$user" />
+                            @endif
+                        </div>
+                    @endforeach
                 </div>
             </div>
             <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                {{ $footer }}
+                <button class="rounded bg-gray-300 px-4 py-2 text-gray-800 hover:bg-gray-400" type="button"
+                    @click="{{ $open }} = false">閉じる</button>
             </div>
         </div>
     </div>
--- src/resources/views/pages/profile/show.blade.php
+++ src/resources/views/pages/profile/show.blade.php
@@ -18,56 +18,8 @@
             <x-users.user-follow-button :user="$user" />
         @endif
 
-        <x-users.user-follow-modal title="フォロワー" open="followerModalOpen">
-            <x-slot name="content">
-                <div class="space-y-4">
-                    @foreach ($user->followers as $follower)
-                        <div class="flex items-center justify-between">
-                            <div>
-                                <a class="font-semibold hover:underline"
-                                    href="{{ route('profile.show', $follower->uid) }}">{{ $follower->name }}</a><br>
-                                <a class="text-sm text-gray-600 hover:underline"
-                                    href="{{ route('profile.show', $follower->uid) }}">{{ '@' . $follower->uid }}</a>
-                            </div>
-                            @if (Auth::id() !== $follower->id)
-                                <x-users.user-follow-button :user="$follower" />
-                            @endif
-                        </div>
-                    @endforeach
-                </div>
-            </x-slot>
-            <x-slot name="footer">
-                <button class="rounded bg-gray-300 px-4 py-2 text-gray-800 hover:bg-gray-400"
-                    @click="followerModalOpen = false">
-                    閉じる
-                </button>
-            </x-slot>
-        </x-users.user-follow-modal>
+        <x-users.user-list-modal title="フォロワー" :users="$user->followers" :isFollowerModal="true" open="followerModalOpen" />
 
-        <x-users.user-follow-modal title="フォロー中" open="followingModalOpen">
-            <x-slot name="content">
-                <div class="space-y-4">
-                    @foreach ($user->following as $following)
-                        <div class="flex items-center justify-between">
-                            <div>
-                                <a class="font-semibold hover:underline"
-                                    href="{{ route('profile.show', $following->uid) }}">{{ $following->name }}</a><br>
-                                <a class="text-sm text-gray-600 hover:underline"
-                                    href="{{ route('profile.show', $following->uid) }}">{{ '@' . $following->uid }}</a>
-                            </div>
-                            @if (Auth::id() !== $following->id)
-                                <x-users.user-follow-button :user="$following" />
-                            @endif
-                        </div>
-                    @endforeach
-                </div>
-            </x-slot>
-            <x-slot name="footer">
-                <button class="rounded bg-gray-300 px-4 py-2 text-gray-800 hover:bg-gray-400"
-                    @click="followingModalOpen = false">
-                    閉じる
-                </button>
-            </x-slot>
-        </x-users.user-follow-modal>
+        <x-users.user-list-modal title="フォロー中" :users="$user->following" :isFollowerModal="false" open="followingModalOpen" />
     </div>
 </x-app-layout>
