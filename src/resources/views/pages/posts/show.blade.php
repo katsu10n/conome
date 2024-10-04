1254bbea6ef59f409a8cf285768644ed78ac94ff
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-10-04 21:28:48 +0900

    ui/add: 投稿詳細ページに中央ヘッダーを追加

--- src/resources/views/pages/posts/show.blade.php
+++ src/resources/views/pages/posts/show.blade.php
@@ -1,4 +1,8 @@
 <x-app-layout>
+    <x-slot name="backButton">
+        <x-common.button-back :main="$post->title" :sub="$post->created_at->format('Y年n月j日 H:i')" />
+    </x-slot>
+
     <div class="rounded-lg bg-white">
         <x-posts.post-list :post="$post" :isLink="false" :current-user-id="$currentUserId" />
         <x-comments.comment-form :post="$post" />
