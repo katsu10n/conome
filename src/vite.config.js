51c2365c18d7c8c127c3ef921633558c65b99ac3
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-10-05 22:18:05 +0900

    chore: ホットリロードの速度を改善

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,27 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+  optimizeDeps: {
+    include: ['frequently-used-dependencies'],
+  },
+
+  cacheDir: '.vite-cache',
+
+  server: {
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+    },
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+  },
+
+  build: {
+    sourcemap: false,
+  },
+
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
