a8fa16eda0ca8ac36f9e4e90ed8ab95a44ac7288
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-10-05 19:05:49 +0900

    ui/upd: 設定ページのヘッダーの余白を調整

--- src/resources/views/components/common/button-back.blade.php
+++ src/resources/views/components/common/button-back.blade.php
@@ -1,6 +1,6 @@
 @props(['main', 'sub' => ''])
 
-<div class="{{ $sub ? '' : 'pt-1' }} flex w-full items-center">
+<div class="{{ $sub ? '' : '-mt-2' }} flex w-full items-center">
     <a class="mr-4 inline-block flex-shrink-0 rounded-lg border border-transparent p-2 hover:border-gray-300 hover:bg-gray-100"
         href="{{ session('previous_url', url()->previous()) }}" onclick="event.preventDefault(); window.history.back();">
         <x-icons.icon-back class="h-6 w-6" />
