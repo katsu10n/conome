a21d41b35d0a71225a4cc47e47b21c2f9dfd4a9c
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-10-06 14:19:53 +0900

    ui/add: 投稿作成ボタンのアイコンを追加

--- src/resources/views/components/posts/post-form-modal.blade.php
+++ src/resources/views/components/posts/post-form-modal.blade.php
@@ -1,8 +1,9 @@
 <div x-data="{ open: false }" @keydown.escape="open = false">
     <button
-        class="text-whiteshadow-md hover:bg-main-dark focus:ring-main-dark w-full rounded-full bg-main px-6 py-4 text-center text-base font-semibold text-bc transition duration-200 ease-in focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-200"
+        class="flex w-full justify-center gap-1 rounded-full bg-main-light px-6 py-4 text-center text-base font-semibold text-bc transition duration-200 ease-in hover:bg-main focus:outline-none focus:ring-2 focus:ring-main-dark focus:ring-offset-2 focus:ring-offset-blue-200"
         type="button" @click="open = !open">
-        投稿する
+        <span>投稿する</span>
+        <x-icons.icon-pencil class="relative top-[4px] h-4 w-4" />
     </button>
 
     <x-common.modal-card title="投稿を作成" open="open">
