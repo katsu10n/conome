33e98c3de85546cbd275170b7f1c1cfed9cce144
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-10-06 13:27:50 +0900

    ui/upd: ドロップダウンメニューのアイコンを変更

---
+++ src/resources/views/components/icons/icon-arrow-down.blade.php
@@ -0,0 +1,5 @@
+@props(['class' => ''])
+
+<x-icons.base-icon :class="$class" {{ $attributes }}>
+    <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+</x-icons.base-icon>
--- src/resources/views/components/posts/post-header-nav-link.blade.php
+++ src/resources/views/components/posts/post-header-nav-link.blade.php
@@ -1,7 +1,7 @@
 @props(['active' => false])
 
 @php
-    $classes = $active ? 'border-main font-bold' : 'border-slate-200 hover:border-gray-light hover:text-main-light';
+    $classes = $active ? 'border-main font-bold text-main' : 'border-slate-200 hover:border-gray-light hover:text-main';
 @endphp
 
 <a {{ $attributes->merge(['class' => "$classes inline-block w-full p-4 border-b-2 text-sm transition-all"]) }}>
