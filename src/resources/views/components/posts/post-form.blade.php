2f20b05a2c7d0c5ed16ad42d11791ebbc103cf96
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-10-06 22:01:13 +0900

    ui/upd: プロフィール編集フォームのデザインを更新

--- src/resources/views/components/posts/post-form.blade.php
+++ src/resources/views/components/posts/post-form.blade.php
@@ -10,14 +10,13 @@
         @endforeach
     </select>
     <input
-        class="w-full flex-1 appearance-none rounded-lg border border-gray px-4 py-2 text-base placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-main"
+        class="w-full flex-1 appearance-none rounded-lg border border-gray px-4 py-2 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-main"
         id="title" name="title" type="text" placeholder="タイトル" required />
     <textarea
-        class="w-full flex-1 resize-none appearance-none rounded-lg border border-gray px-4 py-2 text-base placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-main"
-        id="content" name="content" placeholder="内容" rows="4" required>
-    </textarea>
+        class="w-full flex-1 resize-none appearance-none rounded-lg border border-gray px-4 py-2 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-main"
+        id="content" name="content" placeholder="内容" rows="4" required></textarea>
     <button
-        class="flex w-full justify-center gap-1 rounded-lg bg-main-light px-4 py-2 text-center text-base font-bold text-bc shadow-md transition duration-200 ease-in hover:bg-main"
+        class="flex w-full justify-center gap-1 rounded-lg bg-main-light px-4 py-2 font-bold text-bc shadow-md transition duration-200 ease-in hover:bg-main"
         type="submit">
         <span>投稿する</span>
         <x-icons.icon-post-add class="relative top-[3px] w-5" />
