6a5ab0dc050392d797dbfe1cf737da8124ee253e
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-10-13 18:19:16 +0900

    ui/upd:【投稿一覧】該当する投稿がない場合のメッセージを追加

--- src/resources/views/pages/posts/index.blade.php
+++ src/resources/views/pages/posts/index.blade.php
@@ -4,4 +4,7 @@
             <x-posts.post-item :post="$post" :isLink="true" :current-user-id="$currentUserId" />
         @endforeach
     </ul>
+    @if ($posts->count() == 0)
+        <p class="mt-8 text-center text-xl font-bold">投稿がありません</p>
+    @endif
 </x-app-layout>
