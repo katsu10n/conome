b0e87f43fe78ab9a2e12fc73e81a0c2a94bf221f
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-10-15 00:13:36 +0900

    feat/upd:【コメント削除】確認モーダルを追加

--- src/resources/views/components/comments/comment-nav-dropdown.blade.php
+++ src/resources/views/components/comments/comment-nav-dropdown.blade.php
@@ -1,32 +1,33 @@
-<x-common.dropdown align="right" width="w-60">
-    <x-slot name="trigger">
-        <button
-            class="inline-flex items-center gap-4 transition duration-150 ease-in-out hover:text-main focus:outline-none">
-            <x-icons.icon-arrow-dropdown class="w-6" />
-        </button>
-    </x-slot>
+<div x-data="{ deleteConfirmOpen: false }">
+    <x-common.dropdown align="right" width="w-60">
+        <x-slot name="trigger">
+            <button
+                class="inline-flex items-center gap-4 transition duration-150 ease-in-out hover:text-main focus:outline-none">
+                <x-icons.icon-arrow-dropdown class="w-6" />
+            </button>
+        </x-slot>
 
-    <x-slot name="content">
-        @if ($comment->user_id === Auth::id())
-            <x-common.dropdown-link>
-                <form class="flex w-full items-center text-red-600 hover:text-red-800"
-                    action="{{ route('comments.destroy', $comment) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button class="flex w-full items-center" type="submit">
+        <x-slot name="content">
+            @if ($comment->user_id === Auth::id())
+                <x-common.dropdown-link>
+                    <button class="flex w-full items-center text-red-600 hover:text-red-800"
+                        @click="deleteConfirmOpen = true">
                         <x-icons.icon-trash class="w-6" />
                         <span class="ml-2">コメントを削除する</span>
                     </button>
-                </form>
-            </x-common.dropdown-link>
-        @endif
+                </x-common.dropdown-link>
+            @endif
 
-        @if ($comment->user_id !== Auth::id())
-            <x-common.dropdown-link>
-                <x-icons.icon-other class="w-6" />
-                {{ __('＠ さんをフォローする（未）') }}
-            </x-common.dropdown-link>
-        @endif
+            @if ($comment->user_id !== Auth::id())
+                <x-common.dropdown-link>
+                    <x-icons.icon-other class="w-6" />
+                    {{ __('＠ さんをフォローする（未）') }}
+                </x-common.dropdown-link>
+            @endif
+        </x-slot>
+    </x-common.dropdown>
 
-    </x-slot>
-</x-common.dropdown>
+    <x-common.modal-card-confirm open="deleteConfirmOpen" :action="route('comments.destroy', $comment)" method="DELETE" confirmText="削除する">
+        このコメントを削除してもよろしいですか？この操作は取り消せません。
+    </x-common.modal-card-confirm>
+</div>
