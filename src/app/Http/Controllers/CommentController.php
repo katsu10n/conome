fe2d276ed7bbe9c731186dcfb3a4fa906ebcf3d3
katsu10n <172394922+katsu10n@users.noreply.github.com>
2024-10-14 12:07:00 +0900

    refactor:【コメント作成】バリデーションを分割、コードをシンプルに

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -2,26 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCommentRequest;
 use App\Models\Comment;
 use App\Models\Post;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    public function store(Request $request, Post $post)
+    public function store(StoreCommentRequest $request, Post $post)
     {
-        $request->validate([
-            'content' => 'required|max:1000',
-        ]);
+        $post->comments()->create(
+            $request->validated() + ['user_id' => Auth::id()]
+        );
 
-        $comment = new Comment();
-        $comment->content = $request->content;
-        $comment->user_id = Auth::id();
-        $comment->post_id = $post->id;
-        $comment->save();
-
-        return redirect()->route('posts.show', ['uid' => $post->user->uid, 'post' => $post]);
+        return back()->with('success', 'コメントを投稿しました');
     }
 
     public function destroy(Comment $comment)
